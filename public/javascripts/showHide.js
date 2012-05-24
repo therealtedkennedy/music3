ca5262799287f282f20cc1e67c436f213d7e0efa
Ted Kennedy <therealtedkennedy@gmail.com>
2012-05-23 21:42:04 -0400

    Buttons working correctly.  Going to give Jplayer a try.  Going to resist doing play lists......maybe....if it works.

--- public/javascripts/showHide.js
+++ public/javascripts/showHide.js
@@ -20,12 +20,12 @@
         var options = $.extend(defaults, options);
 
         $(this).click(function () {	
-           
-             $('.toggleDiv').slideUp(options.speed, options.easing);	
+             var toggleDiv = $(this).attr('rel');
+             $('.toggleDiv').not(toggleDiv).slideUp(options.speed, options.easing);
 			 // this var stores which button you've clicked
              var toggleClick = $(this);
 		     // this reads the rel attribute of the button to determine which div id to toggle
-		     var toggleDiv = $(this).attr('rel');
+
 		     // here we toggle show/hide the correct div at the right speed and using which easing effect
 		     $(toggleDiv).slideToggle(options.speed, options.easing, function() {
 		     // this only fires once the animation is completed
