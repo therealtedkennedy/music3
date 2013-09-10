0cf27b9666edcaa988a07b660291305667da8240
Ted Stephen <therealtedkennedy@gmail.com>
2013-09-09 22:07:26 -0400

    emails working in on dev.  Moved style sheet

--- app/mailers/first_mailer.rb
+++ app/mailers/first_mailer.rb
@@ -2,9 +2,11 @@ class FirstMailer < ActionMailer::Base
   default from: "no-replay@firmex.com"
 
 
-  def thank_you_download(user)
+  def thank_you_download(user,album,artist)
 	  logger.info "in thank you download email"
 	  @user = user
+	  @album = album
+	  @artist = artist
 	  @url  = 'http://www.threerepeater.com/login'
 	  mail(to: @user.email, subject: 'Your download')
   end
