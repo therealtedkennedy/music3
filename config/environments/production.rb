e53e1671543d310b026f7d34305dce01c85618b2
Ted Stephen <therealtedkennedy@gmail.com>
2013-09-09 22:20:53 -0400

    fixing production eviroment to send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,16 +71,21 @@ Music3::Application.configure do
     )
 
 
-	  #email config
-	config.action_mailer.delivery_method = :sendmail
-	  # Defaults to:
-	  # config.action_mailer.sendmail_settings = {
-	  #   location: '/usr/sbin/sendmail',
-	  #   arguments: '-i -t'
-	  # }
-	config.action_mailer.perform_deliveries = true
+	config.action_mailer.default_url_options = { :host => 'http://threerepeater.com'}
+
+	# Don't care if the mailer can't send
 	config.action_mailer.raise_delivery_errors = true
-	config.action_mailer.default_options = {from: 'no-reply@threerepeater.com'}
+
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+			address: "smtp.gmail.com",
+			port: 587,
+			domain: "gmail.com",
+			authentication: "plain",
+			enable_starttls_auto: true,
+			user_name: 'threerepeater@gmail.com',
+			password:"three1234!!"
+	}
   end
 
 
