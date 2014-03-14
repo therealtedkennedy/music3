ed3857e2ce58d5334597903d3c515b5e4e79ea67
Ted <therealtedkennedy@gmail.com>
2014-03-13 23:19:39 -0400

    more testing.

--- config/environments/dev_kennedy.rb
+++ config/environments/dev_kennedy.rb
@@ -1,7 +1,7 @@
 Music3::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # The production environment is meant for finished, "live" apps.
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
