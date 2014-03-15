2fd9f38109cd00a93588f5b8d0b78efdf81818e3
Ted <therealtedkennedy@gmail.com>
2014-03-15 13:28:53 -0400

    changed song liecence date to release date

--- db/migrate/20140303030911__change_name_of_licence_date_to_release_date.rb
+++ db/migrate/20140303030911__change_name_of_licence_date_to_release_date.rb
@@ -1,7 +1,7 @@
 class ChangeNameOfLicenceDateToReleaseDate < ActiveRecord::Migration
 
   def self.up
-    rename_column :songs, :song_licence_date, :release_date
+    rename_column :songs, :release_date, :release_date
   end
 
   def self.down
